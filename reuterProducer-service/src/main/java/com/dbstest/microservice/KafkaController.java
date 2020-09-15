ee106e4fe6434f24bd92651549f4c5ef22460a4f
laxmi1707 <47939362+laxmi1707@users.noreply.github.com>
2020-09-15 13:12:33 +0800

    producer service

---
+++ reuterProducer-service/src/main/java/com/dbstest/microservice/KafkaController.java
@@ -0,0 +1,28 @@
+package com.dbstest.microservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dbstest.producer.service.Producer;
+
+@RestController
+@RequestMapping(value = "/kafka")
+public class KafkaController {
+
+	private final Producer producer;
+	
+	@Autowired
+	public KafkaController(Producer producer) {
+		this.producer = producer;
+	}
+	
+	@PostMapping(value = "/publish")
+	public void sendMessageToKafkaTopic(@RequestBody String message){
+		this.producer.sendMessage(message);
+	}
+	
+}
